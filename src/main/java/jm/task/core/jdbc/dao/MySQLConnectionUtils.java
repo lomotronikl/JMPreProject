acae4343dd6924c8448cd7ea702dadcb3d45846e
lomotronikl@gmail.com <bpwAxe08>
2021-08-14 22:44:49 +0800

    1.1.3 done

--- src/main/java/jm/task/core/jdbc/dao/MySQLConnectionUtils.java
+++ src/main/java/jm/task/core/jdbc/dao/MySQLConnectionUtils.java
@@ -6,31 +6,19 @@ import java.sql.SQLException;
 
 public class MySQLConnectionUtils {
 
-public static Connection getMySQLConnection() throws SQLException,
-            ClassNotFoundException {
+public static Connection getMySQLConnection() throws SQLException {
         String hostName = "localhost";
-
         String dbName = "preprjdb";
         String userName = "web";
         String password = "12345678";
-
         return getMySQLConnection(hostName, dbName, userName, password);
     }
 
     public static Connection getMySQLConnection(String hostName, String dbName,
                                                 String userName, String password) throws SQLException{
-
-       // Class.forName("com.mysql.jdbc.Driver");
-
-        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName
-              //  + "?verifyServerCertificate=false&useSSL=true&requireSSL=true";
-             //   +  "?enabledTLSProtocols=TLSv1.2"
-             //+ "?useSSL=NO"
-            ;
-        Connection conn = DriverManager.getConnection(connectionURL, userName,
+        String connectionURL = "jdbc:mysql://" + hostName + ":3306/" + dbName;
+        return DriverManager.getConnection(connectionURL, userName,
                 password);
-
-        return conn;
     }
 
 }
